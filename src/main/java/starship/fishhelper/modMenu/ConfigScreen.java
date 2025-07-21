44680a808bc4a9835e0f06416db6f0f9dd9d8044
shuqishen <1481212714@qq.com>
2025-07-21 13:38:52 +0800

    1.21.5-1.21.7

---
+++ src/main/java/starship/fishhelper/modMenu/ConfigScreen.java
@@ -0,0 +1,38 @@
+package starship.fishhelper.modMenu;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
+import starship.fishhelper.MCCIFishHelper;
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import me.shedaniel.clothconfig2.api.ConfigCategory;
+import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
+
+
+public class ConfigScreen {
+
+    public static Screen buildScreen(MCCIFishHelper fishHelper, Screen parent) {
+        ConfigBuilder builder = ConfigBuilder.create()
+                .setParentScreen(parent)
+                .setTitle(Text.translatable("title.fishhelper.config"));
+
+        ConfigCategory category = builder.getOrCreateCategory(Text.translatable("category.fishhelper.general"));
+        ConfigEntryBuilder entryBuilder = builder.entryBuilder();
+
+        ConfigData config = fishHelper.getConfig();
+
+        category.addEntry(entryBuilder.startBooleanToggle(Text.translatable("option.fishhelper.compact_fishmsg"), config.enableCompactFishmsg)
+                .setDefaultValue(true)
+                .setSaveConsumer(newValue -> config.enableCompactFishmsg = newValue)
+                .build());
+
+        category.addEntry(entryBuilder.startBooleanToggle(Text.translatable("option.fishhelper.trevor_opener"), config.enableTreasureReciMsg)
+                .setDefaultValue(false)
+                .setSaveConsumer(newValue -> config.enableTreasureReciMsg = newValue)
+                .build());
+
+        builder.setSavingRunnable(fishHelper::saveConfig);
+
+        return builder.build();
+    }
+
+}
